8f72e52e0bbc006a723f5d08e7c8916ce308606e
jdpenuliar <jaydz.penuliar@gmail.com>
2016-12-11 15:22:26 -0800

    first push of completed one from tut

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry porter', pages: 39 },
+    { title: 'hha book', pages: 85 },
+    { title: 'haha', pages: 1 }, 
+  ];
+}
